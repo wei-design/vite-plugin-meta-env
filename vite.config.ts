5401e0f93d3551a321f48061995e7d1bb3b96323
wforguo <wforguo@qq.com>
2023-07-14 20:57:55 +0800

    feat: 添加eslint和prettier，并格式化代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter'
+    build: {
+        lib: {
+            entry: './lib/main.ts',
+            name: 'Counter',
+            fileName: 'counter'
+        }
     }
-  }
 })
